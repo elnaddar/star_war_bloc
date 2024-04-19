82312dc744d4306e4abd4f7de756634136967f58
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-04-19 18:23:15 +0200

    [SOLVED] const list in states was causing problem.

--- lib/logic/likes_state.dart
+++ lib/logic/likes_state.dart
@@ -2,18 +2,18 @@ import 'package:star_war_bloc/models/swapi_object.dart';
 
 abstract class LikesState {
   final List<SwapiObject> likes;
-  const LikesState({List<SwapiObject>? likes}) : likes = likes ?? const [];
+  LikesState({List<SwapiObject>? likes}) : likes = likes ?? [];
 }
 
 class LikesInitial extends LikesState {
-  const LikesInitial();
+  LikesInitial();
 }
 
 class LikesLoaded extends LikesState {
-  const LikesLoaded(List<SwapiObject> likes) : super(likes: likes);
+  LikesLoaded(List<SwapiObject> likes) : super(likes: likes);
 }
 
 class LikesError extends LikesState {
   final String message;
-  const LikesError(this.message);
+  LikesError(this.message);
 }
